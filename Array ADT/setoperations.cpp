1c4dcfeb87ce7499b5de986ab6f764ac028be6ff
Anuj054 <anujc054@gmail.com>
2024-04-18 23:30:03 +0530

    union

--- Array ADT/setoperations.cpp
+++ Array ADT/setoperations.cpp
@@ -17,21 +17,25 @@ for(i=0;i<arr.length;i++)
 cout<<arr.A[i] <<endl;
 
 }
-struct Array* merge2array(struct Array *arr1,struct Array *arr2){
+struct Array* Union(struct Array *arr1,struct Array *arr2){
     int i,j,k;
     i=j=k=0;
     struct Array *arr3=(struct Array *)malloc(sizeof(struct Array));
     while(i<arr1->length &&  j<arr2->length){
         if(arr1->A[i]<arr2->A[j])
         arr3->A[k++]=arr1->A[i++];
-        else
+        else if(arr2->A[j]<arr1->A[i])
          arr3->A[k++]=arr2->A[j++];
+        else{
+        arr3->A[k++]=arr1->A[i++];
+         j++;
+            }
     }
     for(;i<arr1->length;i++)
     arr3->A[k++]=arr1->A[i];
     for(;j<arr2->length;j++)
     arr3->A[k++]=arr2->A[j];
-arr3->length=arr1->length+arr2->length;
+arr3->length=k;
 arr3->size=10;
 return arr3;
 }
@@ -42,6 +46,6 @@ int main(){
  struct Array arr1={{2,6,10,15,25},10,5};
  struct Array arr2={{3,6,7,15,20},10,5};
  struct Array *arr3;
- arr3=merge2array(&arr1,&arr2);
+ arr3=Union(&arr1,&arr2);
 Display(*arr3);
 };
